d04c494fe8ef4fa4d22b9ae6a88a7dd1e1b3d8ae
ngwenyi007 <francismatu8@gmail.com>
2025-07-15 00:32:12 +0300

    Added PWA manifest and service worker

--- static/service-worker.js
+++ static/service-worker.js
@@ -9,26 +9,24 @@ const urlsToCache = [
   '/static/manifest.json'
 ];
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        return cache.addAll(urlsToCache);
-      })
+self.addEventListener('install', function (e) {
+  console.log('Service Worker installed');
+  e.waitUntil(
+    caches.open('youthapp-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/static/styles.css',
+        '/static/icons/icon-192.png',
+        '/static/icons/icon-512.png'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        return response || fetch(event.request);
-      })
-  );
-});
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow('/')
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
   );
 });
