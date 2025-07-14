99af6303180ca4616bf03d7ac592c916b69c3fd0
ngwenyi007 <francismatu8@gmail.com>
2025-07-15 00:46:30 +0300

    Add service worker and register it

--- static/service-worker.js
+++ static/service-worker.js
@@ -9,24 +9,15 @@ const urlsToCache = [
   '/static/manifest.json'
 ];
 
-self.addEventListener('install', function (e) {
-  console.log('Service Worker installed');
-  e.waitUntil(
-    caches.open('youthapp-cache').then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/static/styles.css',
-        '/static/icons/icon-192.png',
-        '/static/icons/icon-512.png'
-      ]);
-    })
-  );
+self.addEventListener('install', function (event) {
+  console.log('[ServiceWorker] Installed');
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', function (event) {
+  console.log('[ServiceWorker] Activated');
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(fetch(event.request));
 });
